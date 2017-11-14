82814bbd165b1eded19588e0d43ba6aaf606b064
bigrushdog <randall.rushing@gmail.com>
2017-11-14 01:30:47 -0800

    DUI: Initial checkin for Oreo [3/7]
    Change-Id: I5ea7016c221c0e85752180cd784ec56f715e2c02

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -179,4 +179,9 @@ public class Settings extends SettingsActivity {
     public static class SupportDashboardActivity extends SettingsActivity {}
 
     public static class crDroidSettingsLayoutActivity extends SettingsActivity {}
+
+    public static class NavigationSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class FlingSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class PulseSettingsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -130,6 +130,11 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.crdroid.settings.crDroidSettings;
 import com.crdroid.settings.crDroidSettingsLayout;
 
+import com.crdroid.settings.fragments.FlingSettings;
+import com.crdroid.settings.fragments.NavigationBar;
+import com.crdroid.settings.fragments.PulseSettings;
+import com.crdroid.settings.fragments.SmartbarSettings;
+
 public class SettingsGateway {
 
     /**
@@ -249,6 +254,10 @@ public class SettingsGateway {
             LockscreenDashboardFragment.class.getName(),
             crDroidSettings.class.getName(),
             crDroidSettingsLayout.class.getName(),
+            NavigationBar.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -288,5 +297,9 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
+            Settings.NavigationSettingsActivity.class.getName(),
+            Settings.FlingSettingsActivity.class.getName(),
+            Settings.SmartbarSettingsActivity.class.getName(),
+            Settings.PulseSettingsActivity.class.getName()
     };
 }
